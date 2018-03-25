484a2cc78d70cdf70345773156c323c954e3e219
Marissa Tamaki <tamaki.marissa@gmail.com>
2018-03-24 22:58:24 -0700

    Completed Video 2 (#2)

--- egghead_learning_react/src/App.js
+++ egghead_learning_react/src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+// * Method 1 to Creating Components - State Function Component
+// * all React Components must start with a captial letter to differ from HTML elements
+class App extends React.Component {
+  render() {
+    // note: in jsx className = class in HTML
+    // equivalent of doing React.createElement('h1',    null, 'Hello World')
+    //                 -->       createElement(element, prop,  String)
+    return <h1>Hello World </h1>
+  }
+}
 
-export default App;
+
+// * Method 2 to Creating Components - Stateless Function Component 
+// goes straight to the JSX
+// const App = () => <h1>Hello</h1>
+
+
+export default App
