4a6dc510484a48aea663b127a90c7451d9501937
Marissa Tamaki <tamaki.marissa@gmail.com>
2018-03-25 15:17:53 -0700

    Completed Video 11 - Understand the React Component Lifecycle Methods (#2)

--- egghead_learning_react/src/App.js
+++ egghead_learning_react/src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// * Component 1
+{/* 
+    - Will mount and unmount the component 
+    - needs React DOM 
+    - ie. essentially controls if the App component can be visualized or not
+*/}
+class Wrapper extends React.Component {
+    mount() {
+        ReactDOM.render(<App />, document.getElementById('a'))
+    }
+    
+    unmount() {
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="a"></div>
+            </div>
+        )
+    }
+}
+
 class App extends React.Component {
     
     // State is a collection of values that are meant to be updated by our component
@@ -14,11 +38,14 @@ class App extends React.Component {
             txt: 'Hello World',
             currentEvent: '--',
             refA: '',
-            refB: ''
+            refB: '',
+            lifecycleVal: 0
+
         }
         this.updateTxt= this.updateTxt.bind(this)
         this.updateCurrentEvent = this.updateCurrentEvent.bind(this)
         this.updateRef = this.updateRef.bind(this)
+        this.updateLifecycle = this.updateLifecycle.bind(this)
     }
 
     // update method that will update that value of text in our input box
@@ -50,57 +77,84 @@ class App extends React.Component {
         })
     }
 
-  render() {
-    // similar to props we use {} for state
-    return (
-        <div>
-            <div>
-                <h1>{this.state.txt}</h1>
-                {/* where we had our input, we render a Widget Component
-                    and pass in an update prop (the method) */}
-                <Widget update={this.updateTxt}/>
-                
-                {/* We want to pass Text Content into a prop */}
-                <Button> <Heart / > React </Button>
-            </div>
-            <hr /> {/* ---------------------------------- */}
-            <div>
-                <h1>Current Event: {this.state.currentEvent}</h1>
-                <textarea
-                    onKeyPress={this.updateCurrentEvent}
-                    onCopy={this.updateCurrentEvent}
-                    onCut={this.updateCurrentEvent}
-                    onPaste={this.updateCurrentEvent}
-                    onFocus={this.updateCurrentEvent}
-                    onBlur={this.updateCurrentEvent}
-                    onDoubleClick={this.updateCurrentEvent}
-                />
-            </div>
-            <hr/> {/* ---------------------------------- */}
-            <div>
-                <input
-                    /* ref can also take a callback because it references the node
-                    same as ref={a} */
-                    ref= {node => this.a = node}
-                    type= "text"
-                    onChange={this.updateRef}
-                />
-                    State of A: {this.state.a}
-                <hr/>
+    updateLifecycle() {
+        this.setState({
+            lifecycleVal: this.state.lifecycleVal + 1
+        })
+    }
 
-                
-                <Input
-                    /* Can Reference the instance of another component */
-                    ref= {component => this.b = component}
-                    update={this.updateRef}
-                />
-                    State of B: {this.state.b}
-            </div>
+    componentWillMount() {
+        // Fires off right before a component is mounted to a DOM
+        // lets us know it is guaranteed to work properly
+        // will only execute once
+        console.log('Component Will Mount')
+    }
 
-        </div>
-    )
-  }
+
+    render() {
+        console.log('render');
+        // similar to props we use {} for state
+        return (
+            <div>
+                <div>
+                    <h1>{this.state.txt}</h1>
+                    {/* where we had our input, we render a Widget Component
+                        and pass in an update prop (the method) */}
+                    <Widget update={this.updateTxt}/>
+                    
+                    {/* We want to pass Text Content into a prop */}
+                    <Button> <Heart / > React </Button>
+                </div>
+                <hr /> {/* ---------------------------------- */}
+                <div>
+                    <h1>Current Event: {this.state.currentEvent}</h1>
+                    <textarea
+                        onKeyPress={this.updateCurrentEvent}
+                        onCopy={this.updateCurrentEvent}
+                        onCut={this.updateCurrentEvent}
+                        onPaste={this.updateCurrentEvent}
+                        onFocus={this.updateCurrentEvent}
+                        onBlur={this.updateCurrentEvent}
+                        onDoubleClick={this.updateCurrentEvent}
+                    />
+                </div>
+                <hr/> {/* ------------ Video 10 ---------------- */}
+                <div>
+                    <input
+                        /* ref can also take a callback because it references the node
+                        same as ref={a} */
+                        ref= {node => this.a = node}
+                        type= "text"
+                        onChange={this.updateRef}
+                    />
+                        State of A: {this.state.a}
+                    <Input
+                        /* Can Reference the instance of another component */
+                        ref= {component => this.b = component}
+                        update={this.updateRef}
+                    />
+                        State of B: {this.state.b}
+                </div>
+                <hr/> {/* ------------ Video 11 ---------------- */}
+                <button onClick={this.updateLifecycle}>{this.state.lifecycleVal}</button>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        // Will fire off once the component is mounted to the DOM
+        console.log('Component Did Mount')
+    }
+
+    
+    componentWillUnmount() {
+        // will fire when the component is about to leave the DOM 
+        console.log('Component Will Unmount')
+    }
+
 }
+
+
 
 class Input extends React.Component {
     render() {
@@ -125,4 +179,4 @@ class Heart extends React.Component {
     }
 }
 
-export default App
+export default Wrapper
